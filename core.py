6358c9e29a06b519bef6142fbbe2b0296f61ad85
Felipe Sampaio <o22466244@gmail.com>
2025-11-26 15:49:14 -0300

    code init

---
+++ core.py
@@ -0,0 +1,14 @@
+#dict = {
+#    "felipe" : {"idade" : 9, "email" : "teste@teste.com"}
+#   }
+
+dict = {}
+
+def verificar_credenciais(nome, idade, email):
+    pass
+
+def add_no_dict(nome, idade, email):
+    pass
+
+def listar():
+    pass
